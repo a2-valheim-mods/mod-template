0b7798210a54d5cf40a73f574081732810e3f7b6
Anton <anton.allers@gmail.com>
2026-01-30 01:58:21 +0100

    project template

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("mod name")]
+[assembly: AssemblyDescription("mod description")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("mod name")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("b004e837-fb81-9f8e-d46b-272436a0904f")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
---
+++ Properties/PluginInfo.cs
@@ -0,0 +1,9 @@
+﻿
+internal static class PluginInfo
+{
+    public const string PluginGUID = "prefix.modname";
+    public const string PluginName = "mod name";
+    public const string PluginDescription = "mod description";
+    public const string PluginSemanticVersion = "0.0.0";
+    public const string PluginFullVersion = "0.0.0.0";
+}
